1535998adb5d7cb2a4fe7ebc649328a553577442
_Duck-Noris_ <rayan.boughalia@gmail.com>
2025-06-15 18:36:29 +0200

    Add PrettyTable class and grid display functionality; update grid interaction and styles

--- BattleShip_Engine/app.js
+++ BattleShip_Engine/app.js
@@ -2,6 +2,31 @@ const root = document.documentElement;
 const gridContainer = document.getElementById("grid_container");
 const generateBoardBtn = document.getElementById("generate_board_btn");
 
+class PrettyTable {
+  constructor() {
+    this._rows = [];
+  }
+
+  addRow(rowData) {
+    this._rows.push(rowData);
+  }
+
+  display() {
+    const rowStrings = this._rows.map((row) => row.join(" "));
+    console.log(rowStrings.join("\n") + "\n");
+  }
+}
+
+function displayGrid(grid) {
+  const table = new PrettyTable();
+
+  grid.forEach((row) => {
+    table.addRow(row.map((cell) => String(cell).padEnd(2)));
+  });
+
+  table.display();
+}
+
 function generateGridHTML(rows, columns) {
   const gridHTML = document.createElement("div");
   gridHTML.classList.add("grid");
@@ -25,6 +50,26 @@ function generateGridHTML(rows, columns) {
   return gridHTML;
 }
 
+function getGridMatrix() {
+  const gridHTML = document.getElementsByClassName("grid-cell");
+  const gridMatrix = [];
+  for (
+    let i = 0;
+    i < gridHTML.length;
+    i += parseInt(root.style.getPropertyValue("--columns"), 10)
+  ) {
+    const row = [];
+    for (let j = 0; j < root.style.getPropertyValue("--columns"); j++) {
+      const cell = gridHTML[i + j];
+      row.push(cell.innerText === "x" ? "x" : 0);
+    }
+    gridMatrix.push(row);
+  }
+  return gridMatrix;
+}
+
+displayGrid(getGridMatrix());
+
 generateBoardBtn.addEventListener("click", () => {
   const sizes = document.querySelectorAll("#size");
   const rows = sizes[0].value;
@@ -33,6 +78,22 @@ generateBoardBtn.addEventListener("click", () => {
   gridContainer.appendChild(generateGridHTML(rows, columns));
 });
 
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("grid-cell")) {
+    // Add your logic for what should happen when a grid cell is clicked
+    // event.target.style.backgroundColor = "red";
+    event.target.innerText = "x";
+    event.target.style.color = "#262624";
+    event.target.style.fontSize = "2rem";
+    event.target.style.backgroundColor = "#ef4444";
+  }
+});
+
 document.addEventListener("contextmenu", (event) => {
-  event.preventDefault();
+  if (event.target.classList.contains("grid-cell")) {
+    // Add your logic for what should happen when a grid cell is clicked
+    event.preventDefault();
+    event.target.innerText = "";
+    event.target.style.backgroundColor = "";
+  }
 });

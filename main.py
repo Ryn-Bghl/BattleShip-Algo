06c219f92367654518382f641708aa5faf16cde0
_Duck-Noris_ <rayan.boughalia@gmail.com>
2025-06-15 15:38:15 +0200

    Refactor display_grid function for improved readability; remove unused functions

--- main.py
+++ main.py
@@ -1,58 +1,41 @@
-from typing import List, Any
+from prettytable import PrettyTable
 
 
 def display_grid(grid):
+    table = PrettyTable()
+    table.hrules = 1
+    table.vrules = 1
+    table.header = False
+
     for row in grid:
-        print(' '.join(str(cell) for cell in row))
+        table.add_row([str(cell).ljust(2) for cell in row])
+
+    print(table, end="\n\n")
 
 
 def rotate_boat(boat):
     return [list(reversed(col)) for col in zip(*boat)]
 
 
-def isplacable(boat, grid):
-    try:
-        for i in range(len(boat)):
-            for j in range(len(boat[i])):
-                if boat[i][j] == 1 and type(grid[i][j]) != int:
-                    return False
-        return True
-    except:
-        return False
-
-
-def get_frequency(grid, boat):
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if isplacable(boat, grid):
-                grid[i][j] += 1
-    return
-
-
-def get_max_width(matrix):
-    return max(len(row) for row in matrix)
-
-
-def set_next_position(boat: List[List[int]], grid: List[List[int]]) -> None:
-    """Set the next position of the boat in the grid."""
-    for i, row in enumerate(boat):
-        for j, cell in enumerate(row):
-            if isplacable(boat, grid):
-                if get_max_width(boat) != get_max_width(grid):
-                    return [[0] + row[:-1] for row in boat]
-                else:
-                    zero_row = [0] * len(boat[0])
-                    return [zero_row] + boat[:-1]
+def get_stats(boat, grid):
+    for n in range(4):
+        for i in range(len(grid) - len(boat) + 1):
+            for j in range(len(grid[i]) - len(boat[0]) + 1):
+                for k in range(len(boat)):
+                    for l in range(len(boat[k])):
+                        grid[i + k][j + l] += boat[k][l]
+        boat = rotate_boat(boat)
+    return grid
 
 
 grid = [[0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]]
 
-boat = [[1, 1, 0],
-        [0, 0, 0],
-        [0, 0, 0]]
+boat = [[0, 1, 0],
+        [1, 1, 1],
+        [0, 1, 0]]
 
-# display_grid(boat)
-set_next_position(boat, grid)
+grid = get_stats(boat, grid)
 display_grid(boat)
+display_grid(grid)
